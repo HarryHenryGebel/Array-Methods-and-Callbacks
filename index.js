c1a445fb341e2ce0134cb28db65e0c9252297178
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-16 06:26:11 -0400

    Complete task 2

--- index.js
+++ index.js
@@ -32,12 +32,11 @@ function findFinalMatchFieldByYear(year, fieldName) {
     console.log(awayName);
 }
 
-/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+/* Task 2: Create a function called getFinals that takes `data` as an
+ * argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+  return data.filter(match => match.Stage === "Final");
 };
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
